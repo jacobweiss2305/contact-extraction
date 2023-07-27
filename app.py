d6592c9705c3cacba6186f337636c755100db210
jacob weiss <jacob.weiss@data-axle.com>
2023-07-27 08:40:58 -0600

    update

--- app.py
+++ app.py
@@ -5,28 +5,65 @@ load_dotenv()
 import streamlit as st
 import pandas as pd
 
+from pydantic import BaseModel, Field
+from typing import Optional
 
+from langchain.chains.openai_functions import (
+    create_openai_fn_chain,
+    create_structured_output_chain,
+)
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import create_extraction_chain
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.agents import create_pandas_dataframe_agent
+from typing import List
+from pydantic import BaseModel
 
-json_schema = {
-    "title": "Person",
-    "description": "Identifying information about a person.",
-    "type": "object",
-    "properties": {
-        "first_name": {"title": "First Name", "description": "The person's first name", "type": "string"},
-        "last_name": {"title": "Last Name", "description": "The person's last name", "type": "string"},
-        "job_title": {"title": "Job Title", "description": "The person's job title", "type": "string"},
-        "company_name": {"title": "Company Name", "description": "The person's company name", "type": "string"},
-        "mobile_number": {"title": "Mobile Number", "description": "The person's mobile number", "type": "string"},
-        "office_number": {"title": "Office Number", "description": "The person's office number", "type": "string"},
-        "business_website": {"title": "Business Website", "description": "The website of the company that the person works for", "type": "string"},
-        "address": {"title": "Address", "description": "The address of the company that the person works for", "type": "string"},
-    },
-    "required": ["first_name", "last_name", "job_title", "company_name", "mobile_number", "office_number", "business_website", "email", "address"]
-}
+import concurrent.futures
 
-llm = ChatOpenAI(model="gpt-4", temperature=0)
+
+
+class ContactInfo(BaseModel):
+    first_name: str
+    last_name: str
+    job_title: str
+    company_name: str
+    mobile_number: str
+    desk_number: str
+    business_website: str
+    email: str
+    address: str
+
+class PersonalInfo(BaseModel):
+    personal_info: List[ContactInfo]
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+def to_lowercase(df):
+    return df.applymap(lambda s: s.lower() if type(s) == str else s)
+
+def get_unique_contacts_with_most_info(df):
+    # Add a column that counts the number of non-null fields for each contact
+    df['info_count'] = df.notnull().sum(axis=1)
+    # Group by 'first_name', 'last_name', 'email', and get the row with max 'info_count' for each group
+    df = df.loc[df.groupby(['first_name', 'last_name', 'email'])['info_count'].idxmax()]
+    # Drop 'info_count' as it's no longer needed
+    df = df.drop(columns='info_count')
+    return df
+
+def split_text_by_from(text):
+    # Split the text by "From:" and strip any leading/trailing spaces from each part
+    split_text = [part.strip() for part in text.split("From:")]
+
+    # Remove the empty string at the beginning caused by the split
+    split_text = [part for part in split_text if part]
+
+    return split_text
+
+def process_email(chain, email):
+    output = chain.run(email)
+    return pd.DataFrame(output.dict()['personal_info'])
 
 def main():
     st.title("AI Contact Extraction App")
@@ -36,12 +73,45 @@ def main():
     if st.button("Extract Contacts"):
             if conversation_input:
 
-                chain = create_extraction_chain(json_schema, llm)
+                prompt_msgs = [
+                    SystemMessage(
+                        content="You are a world class algorithm for extracting information in structured formats."
+                    ),
+                    HumanMessage(
+                        content="Use the given format to extract contact details from the following email (include contacts in Cc:):"
+                    ),
+                    HumanMessagePromptTemplate.from_template("{input}"),
+                    HumanMessage(content="Make sure to answer in the correct format"),
+                ]
+                prompt = ChatPromptTemplate(messages=prompt_msgs)
 
-                output = chain.run(conversation_input)
+                chain = create_structured_output_chain(PersonalInfo, llm, prompt, verbose=True)
 
-                st.write(pd.DataFrame(output))
+                split_emails = split_text_by_from(conversation_input)
 
+                main = []
+                # Use ThreadPoolExecutor to parallelize the processing of emails
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    # Submit each email processing task to the ThreadPoolExecutor
+                    future_to_email = {executor.submit(process_email, chain, email): email for email in split_emails}
+
+                    # Wait for all tasks to complete and get the results
+                    for future in concurrent.futures.as_completed(future_to_email):
+                        email = future_to_email[future]
+                        try:
+                            df = future.result()
+                        except Exception as exc:
+                            df = pd.DataFrame()
+                        
+                        main.append(df)
+
+                st.write("Extraction Results:")
+
+                output = to_lowercase(pd.concat(main, ignore_index=True).drop_duplicates().replace('', None))
+                
+                deduplicated_df = get_unique_contacts_with_most_info(output)
+                
+                st.dataframe(deduplicated_df)
             else:
                 st.warning("Please enter a conversation text.")
 
