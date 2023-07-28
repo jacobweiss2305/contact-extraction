03d52f7203d344883a396d22d0966593f9f12709
jacob weiss <jacob.weiss@data-axle.com>
2023-07-28 07:58:39 -0600

    update

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ from langchain.schema import HumanMessage, SystemMessage
 from langchain.agents import create_pandas_dataframe_agent
 from typing import List
 from pydantic import BaseModel
-
+import utils
 import concurrent.futures
 
-
+st.set_page_config(layout="wide")
 
 class ContactInfo(BaseModel):
     first_name: str
@@ -66,11 +66,24 @@ def process_email(chain, email):
     return pd.DataFrame(output.dict()['personal_info'])
 
 def main():
-    st.title("AI Contact Extraction App")
-    st.write("Enter the conversation text:")
-    conversation_input = st.text_area("Conversation", "")
+    st.title("Contact Finder")
+    st.markdown("")
+    st.markdown("### Paste your message below")
 
-    if st.button("Extract Contacts"):
+    with st.container():
+        conversation_input = st.text_area("", "", height=500)
+
+    cols = st.columns([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) # Split the window into three columns of equal width
+    
+    # Add clear button in the first column
+    if cols[0].button("Clear"):
+        conversation_input = ""  # This sets the conversation_input to an empty string
+        
+    # Leave the second column empty
+    cols[1].write("")
+
+    # Add the extraction button to the third column
+    if cols[9].button("Find Contacts"):
             if conversation_input:
 
                 prompt_msgs = [
@@ -104,14 +117,78 @@ def main():
                             df = pd.DataFrame()
                         
                         main.append(df)
+                
+                st.markdown("")                        
 
-                st.write("Extraction Results:")
+                st.markdown("### Found Contacts")
 
                 output = to_lowercase(pd.concat(main, ignore_index=True).drop_duplicates().replace('', None))
                 
                 deduplicated_df = get_unique_contacts_with_most_info(output)
-                
-                st.dataframe(deduplicated_df)
+
+                contacts = deduplicated_df.to_dict('records')
+
+                output = to_lowercase(pd.concat(main, ignore_index=True).drop_duplicates().replace('', None))
+
+                deduplicated_df = get_unique_contacts_with_most_info(output)
+
+                contacts = deduplicated_df.to_dict('records')
+           
+
+                # The style and start of the contact card container
+                st.markdown("""
+                <style>
+                    .contact-card-container {
+                        display: grid;
+                        grid-template-columns: repeat(3, 1fr);
+                        gap: 20px;
+                        box-sizing: border-box;
+                        padding: 20px;
+                        font-family: 'Arial', sans-serif;
+                    }
+                    .contact-card {
+                        border: 2px solid #ccc;
+                        border-radius: 5px;
+                        padding: 20px;
+                        font-size: 0.85em;
+                        line-height: 1.6;
+                    }
+                </style>
+
+                <div class="contact-card-container">
+                """, unsafe_allow_html=True)
+
+                index = 0
+                cols = st.columns(3)
+
+                for contact in contacts:
+                    with cols[index % 3]:
+                        first_name = contact['first_name'].capitalize()
+                        last_name = contact['last_name'].capitalize()
+                        job_title = contact['job_title'] if pd.notnull(contact['job_title']) else "N/A"
+                        email = contact['email'] if pd.notnull(contact['email']) else "N/A"
+                        mobile_number = contact['mobile_number'] if pd.notnull(contact['mobile_number']) else "N/A"
+
+                        image_url = utils.get_image(f"{first_name} {last_name}")
+
+
+                        st.markdown(f"""
+                            <div class="contact-card">
+                                <img src="{image_url}" alt="Profile Picture" width="100">
+                                <h2>{first_name} {last_name}</h2>
+                                <p><small style="color: grey;">{job_title}</small></p>
+                                <p><strong>Email:</strong> {email}</p>
+                                <p><strong>Phone:</strong> {mobile_number}</p>
+                            </div>
+                        """, unsafe_allow_html=True)
+
+
+                        index += 1
+
+                # End of the contact card container
+                st.markdown("""
+                </div>
+                """, unsafe_allow_html=True)
             else:
                 st.warning("Please enter a conversation text.")
 
--- utils.py
+++ utils.py
@@ -1,60 +1,14 @@
-import pandas as pd
+from langchain.chat_models import ChatOpenAI
+import openai
 
-from langchain.callbacks import get_openai_callback
-from kor import extract_from_documents
+def get_image(name):
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    gender = llm.predict(f"Classify {name} as male or female. If you unsure, refer to female. Your response needs to be male or female.")
 
-# __import__('pysqlite3')
-# import sys
-# import os
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+    response = openai.Image.create(
+    prompt=f"professional business {gender}",
+    n=1,
+    size="1024x1024"
+    )
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join('/workspaces/contact-extraction', 'db.sqlite3'),
-#     }
-# }
-
-def load_conversation(filename):
-
-    with open(filename, 'r', encoding='utf-8') as f:
-        conversation = f.read()
-
-    return conversation
-
-def generate_dataframe(json_data):
-    # Prepare an empty list to store all restaurant data
-    data = []
-
-    for record in json_data:
-        restaurant_list = record.get('data', {}).get('personal_info', [])
-        for restaurant in restaurant_list:
-            # Get details for each restaurant and append it to data
-            data.append([
-                restaurant.get('first_name', ''),
-                restaurant.get('last_name', ''),
-                restaurant.get('job_title', ''),
-                restaurant.get('mobile_number', ''),
-                restaurant.get('desk_number', ''),
-                restaurant.get('email', ''),
-                restaurant.get('company_name', ''),
-                restaurant.get('business_url', ''),  
-                restaurant.get('address', ''),
-
-            ])
-
-    # Convert the list into a DataFrame
-    df = pd.DataFrame(data, columns=[
-        'first_name', 'last_name', 'job_title', 
-        'mobile_number', 'desk_number', 'email', 
-        'company_name', 'business_url', 'address',
-        ])
-
-    return df
-
-async def extract_contacts_from_documents(chain, split_docs):
-
-    with get_openai_callback() as cb:
-        return await extract_from_documents(
-            chain, split_docs, max_concurrency=5, use_uid=False, return_exceptions=True
-        )
+    return response['data'][0]['url']
